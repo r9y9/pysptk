dde86ca4fa1ca635ddce944f137ec7445411b94a
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-09-04 02:21:41 +0900

    add tests for f0

--- tests/test_f0.py
+++ tests/test_f0.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 import numpy as np
-import six
 import pysptk
 from nose.tools import raises
 
@@ -14,12 +13,12 @@ def test_swipe():
 
     np.random.seed(98765)
     fs = 16000
-    hopsize = 80
     x = np.random.rand(16000)
 
-    for otype in six.moves.range(0, 3):
-        yield __test, x, fs, hopsize, otype
+    for hopsize in [40, 80, 160, 320]:
+        for otype in [0, 1, 2]:
+            yield __test, x, fs, hopsize, otype
 
     # unsupported otype
-    yield raises(ValueError)(__test), x, fs, hopsize, -1
-    yield raises(ValueError)(__test), x, fs, hopsize, 3
+    yield raises(ValueError)(__test), x, fs, 80, -1
+    yield raises(ValueError)(__test), x, fs, 80, 3
